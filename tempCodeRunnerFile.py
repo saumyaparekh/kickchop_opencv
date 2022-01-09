cf0b25d692c494246e317562cd8908a9b700ae87
saumyaparekh <59766466+saumyaparekh@users.noreply.github.com>
2022-01-09 23:25:04 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-result = cv.matchTemplate(sc, rwood, cv.TM_CCOEFF_NORMED)
-# min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-# if max_val >= 0.80 :
-#     side = 'R'
-#     py.click(x=1200, y=400, clicks=1, button='left')
